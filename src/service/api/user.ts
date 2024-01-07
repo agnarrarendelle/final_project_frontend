f65b3d1293d2977072e0917896095c48c9972144
agnarrarendelle <danny20001102@gmail.com>
2024-01-07 13:42:48 +0800

    added a method to check if an user is logged in

--- src/service/api/user.ts
+++ src/service/api/user.ts
@@ -1,12 +1,29 @@
-import axios from ".."
+import axios from "..";
 import { ApiResponse, UserResponse } from "../response_types";
 
-export const login = async (name: string, password: string):Promise<ApiResponse<UserResponse>> => {
-    const res = await axios.post("/user/login", {name, password})
-    return res.data
+const RootApiPath = "/user";
+
+export const login = async (
+  name: string,
+  password: string
+): Promise<ApiResponse<UserResponse>> => {
+  const res = await axios.post(`${RootApiPath}/login`, { name, password });
+  return res.data;
 };
 
-export const searchUsers = async(groupId:number, name:string):Promise<ApiResponse<UserResponse[]>>=>{
-    const res = await axios.get(`/user?groupId=${groupId}&name=${name}`)
-    return res.data
-}
+export const searchUsers = async (
+  groupId: number,
+  name: string
+): Promise<ApiResponse<UserResponse[]>> => {
+  const res = await axios.get(`${RootApiPath}?groupId=${groupId}&name=${name}`);
+  return res.data;
+};
+
+export const checkUserLogin = async () => {
+  try {
+    await axios.put(RootApiPath);
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
