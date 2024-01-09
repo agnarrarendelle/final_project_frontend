95822f7c4767352a7d75114b6d73833280556ff3
agnarrarendelle <danny20001102@gmail.com>
2024-01-09 21:47:51 +0800

    added websocket and chat history to vuex states

--- src/state/state.ts
+++ src/state/state.ts
@@ -1,24 +1,29 @@
+import { Client } from "@stomp/stompjs";
 import {
   CategoryResponse,
   TaskResponse,
   UserResponse,
 } from "../service/response_types";
+import { ChatMessage } from "../service/request_types";
 
 export type State = {
   user: UserResponse;
   groupDetails: Map<
     number,
     {
-      id:number
-      name:string;
+      id: number;
+      name: string;
       categories: CategoryResponse[];
       tasks: TaskResponse[];
       users: UserResponse[];
+      chatMessages: ChatMessage[]
     }
   >;
+  websocket: Client | null;
 };
 
 export const state: State = {
   user: { name: "", token: "", id: -1 },
   groupDetails: new Map(),
+  websocket: null,
 };
